53e0895158fa4a78a55f770c88e6328d14798257
Federico Ressi <federico.ressi@elastic.co>
2025-09-01 11:43:04 +0200

    Use UV to create venv. Add pyproject.toml file. Read CSV before starting.

--- Makefile
+++ Makefile
@@ -4,16 +4,24 @@ src_dir := $(patsubst %/,%,$(dir $(makefile_path)))
 
 venv_dir := $(src_dir)/.venv
 venv_python := $(venv_dir)/bin/python3
-venv_command = $(SHELL) -c ". $(venv_dir)/bin/activate; $(1)"
+venv_sh = $(SHELL) -c ". $(venv_dir)/bin/activate; $(1)"
 
-$(venv_python):
-	rm -fR "$(venv_dir)"
-	virtualenv "$(venv_dir)"
-	$(call venv_command,$(venv_python) -m pip install --upgrade -r $(src_dir)/requirements.txt)
+all: venv
+
+clean: clean-venv
 
 venv: $(venv_python)
+
+run: venv
+	uv run esprober.py
 
 clean-venv:
 	rm -fR "$(venv_dir)"
 
-.PHONY: venv clean_venv
+check-uv:
+	@uv --version || echo "Please install uv -> https://github.com/astral-sh/uv?tab=readme-ov-file#installation"
+
+$(venv_python): check-uv
+	uv add --dev .
+
+.PHONY: venv run clean clean-venv check-uv
--- esprober.py
+++ esprober.py
@@ -1,12 +1,12 @@
-import contextlib
 import csv
 import dataclasses
 import datetime
 import functools
+import json
 import logging
 import os
 import time
-from collections.abc import Iterator
+from collections.abc import Iterator, Iterable
 from typing import Any
 
 import elasticsearch
@@ -17,8 +17,13 @@ LOG = logging.getLogger(__name__)
 API_KEY: str | None = os.getenv("ESPROBER_API_KEY", "").strip() or None
 API_URL: str = (os.getenv("ESPROBER_API_URL", "").strip() or "https://overview-elastic-cloud-com.es.us-east-1.aws.found.io:443").rstrip("/")
 
-CSV_FILENAME = os.path.expanduser(os.getenv("ESPROBER_CSV_FILENAME", "esprober.csv"))
 LOG_FILENAME = os.path.expanduser(os.getenv("ESPROBER_LOG_FILENAME", "esprober.log"))
+QUERIES_FILENAME = os.path.expanduser(os.getenv("ESPROBER_QUERIES_FILENAME", "queries.json"))
+RESULTS_FILENAME = os.path.expanduser(os.getenv("ESPROBER_CSV_FILENAME", "results.csv"))
+
+QUERY_INTERVAL: float = max(1., float(os.getenv("ESPROBER_QUERY_INTERVAL", "").strip() or 60.))
+TEST_DURATION: float | None = max(0., float(os.getenv("ESPROBER_TEST_DURATION", "").strip() or 0.)) or None
+REQUEST_TIMEOUT: float = max(1., float(os.getenv("ESPROBER_REQUEST_TIMEOUT", "").strip() or 120.))
 
 
 @dataclasses.dataclass
@@ -27,110 +32,16 @@ class Query:
     path: str
     body: dict[str, Any]
 
-
-QUERIES: list[Query] = [
-    Query(
-        name="service.node.name-wildcard",
-        path="serverless-metrics-*:apm-*,serverless-metrics-*:metrics-apm*",
-        body={
-            "query": {
-                "wildcard": {
-                    "service.node.name": {
-                        "value": "es-es-search*"
-                    }
-                }
-            }
-        },
-    ),
-   Query(
-        name= "service.node.name-term",
-        path="serverless-metrics-*:apm-*,serverless-metrics-*:metrics-apm*",
-        body={
-            "query": {
-                "term": {
-                    "service.node.name": {
-                        "value": "es-es-search-7c46b56686-sdtrl"
-                    }
-                }
-            }
-        }
-    ),
-    Query(
-        name="kubernetes.pod.name-wildcard",
-        path="metrics-*,serverless-metrics-*:metrics-*",
-        body={
-            "query": {
-                "wildcard": {
-                    "kubernetes.pod.name": {
-                        "value": "es-*"
-                    }
-                }
-            }
-        },
-    ),
-    Query(
-        name="kubernetes.pod.name-term",
-        path="metrics-*,serverless-metrics-*:metrics-*",
-        body={
-            "query": {
-                "term": {
-                    "kubernetes.pod.name": {
-                        "value": "es-es-index-564b5c6d45-7hldp"
-                    }
-                }
-            }
-        },
-    ),
-]
-
-
-QUERY_INTERVAL: float = max(1., float(os.getenv("ESPROBER_QUERY_INTERVAL", "").strip() or 60.))
-TEST_DURATION: float | None = max(0., float(os.getenv("ESPROBER_TEST_DURATION", "").strip() or 0.)) or None
-REQUEST_TIMEOUT: float = max(1., float(os.getenv("ESPROBER_REQUEST_TIMEOUT", "").strip() or 120.))
-
-CLIENTS: dict[str, elasticsearch.Elasticsearch] = {}
-
-
-def main():
-    logging.basicConfig(filename=LOG_FILENAME, encoding='utf-8', level=logging.DEBUG)
-
-    test_deadline: float | None = None
-    if TEST_DURATION:
-        test_deadline = time.monotonic() + TEST_DURATION
-
-    durations: dict[str, list[float]] = {}
-    for q in QUERIES:
-        durations[q.name] = []
-
-    with QueryResult.csv_writer() as writer:
-        while not test_deadline or time.monotonic() < test_deadline:
-            for query in QUERIES:
-                # Send a query to elastic search service
-                try:
-                    LOG.info("Executing query '%s'...", query.name)
-                    result = send_query(query)
-                    # Write query results to CSV file
-                    result.write_to(writer)
-
-                    # Aggregate print query stats.
-                    durations[query.name].append(result.duration)
-                    LOG.info("Query '%s' average time: %f seconds", query.name, average(durations[query.name]))
-                except Exception as ex:
-                    LOG.exception("Query '%s' failed: %s", query.name, ex)
-                # Give the service a fair break to reduce its charge
-                LOG.debug("Sleeping %d seconds...", int(QUERY_INTERVAL))
-                time.sleep(QUERY_INTERVAL)
-
-
-class DictWriter(csv.DictWriter):
-
-    def __init__(self, f, fieldnames, *args, **kwargs):
-        super().__init__(f, fieldnames, *args, **kwargs)
-        self.flush = f.flush
-
-    def writerow(self, row):
-        super().writerow(row)
-        self.flush()
+    def send(self) -> 'QueryResult':
+        url = f"{API_URL}/{self.path}".rstrip("/")
+        timestamp = datetime.datetime.strftime(
+            datetime.datetime.now(datetime.timezone.utc),
+            "%Y-%m-%dT%H:%M:%S.%f"
+        )[:-3]
+        start_time = time.monotonic()
+        client(url).search(**self.body)
+        duration = time.monotonic() - start_time
+        return QueryResult(timestamp=timestamp, duration=duration, name=self.name)
 
 
 @dataclasses.dataclass
@@ -139,45 +50,103 @@ class QueryResult:
     name: str
     duration: float
 
-    @classmethod
-    @contextlib.contextmanager
-    def csv_writer(cls) -> Iterator[csv.writer]:
-        out_dir = os.path.dirname(CSV_FILENAME)
-        if out_dir and not os.path.isdir(out_dir):
-            os.makedirs(out_dir)
-        write_header = not os.path.isfile(CSV_FILENAME)
-        with open(CSV_FILENAME, "a", newline="") as csv_file:
-            csv_writer = DictWriter(csv_file, fieldnames=["timestamp", "name", "duration"])
-            if write_header:
-                csv_writer.writeheader()
 
-            yield csv_writer
-            csv_file.flush()
+def main(
+    log_filename: str = LOG_FILENAME,
+    queries_filename: str = QUERIES_FILENAME,
+    results_filename: str = RESULTS_FILENAME
+) -> None:
+    init_logging(log_filename)
 
-    def write_to(self, writer: csv.DictWriter) -> None:
-        writer.writerow(dataclasses.asdict(self))
+    queries = load_queries(queries_filename)
+
+    durations: dict[str, list[float]] = {q.name: [] for q in queries}
+    for result in read_results(results_filename):
+        durations[result.name].append(result.duration)
+    for q in queries:
+        LOG.info("Query '%s' average time: %f seconds", q.name, average(durations[q.name]))
+
+    results = send_queries(queries=queries, durations=durations)
+    write_results(results_filename, results)
 
 
-def send_query(query) -> QueryResult:
-    timestamp = datetime.datetime.strftime(
-        datetime.datetime.now(datetime.timezone.utc),
-        "%Y-%m-%dT%H:%M:%S.%f"
-    )[:-3]
-    start_time = time.monotonic()
-    search(query)
-    duration = time.monotonic() - start_time
-    return QueryResult(timestamp=timestamp, name=query.name, duration=duration)
+def init_logging(filename: str = LOG_FILENAME) -> None:
+    logging.basicConfig(
+        filename=filename,
+        encoding='utf-8',
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+
+def load_queries(filename: str) -> list[Query]:
+    with open(filename) as f:
+        LOG.info("Loading queries from '%s'.", filename)
+        return [Query(**d) for d in json.load(f)]
+
+
+def send_queries(
+    queries: Iterable[Query],
+    durations: dict[str, list[float]],
+    interval: float=QUERY_INTERVAL
+) -> Iterator[QueryResult]:
+    test_deadline: float | None = None
+    if TEST_DURATION:
+        test_deadline = time.monotonic() + TEST_DURATION
+
+    for query in queries:
+        if test_deadline and time.monotonic() < test_deadline:
+            LOG.info("Test duration expired.", query.name)
+            break
+
+        LOG.info("Sending query '%s'...", query.name)
+        try:
+            result = query.send()
+        except Exception as ex:
+            LOG.exception("Query '%s' failed: %s", query.name, ex)
+        else:
+            durations[query.name].append(result.duration)
+            LOG.info("Query '%s' average time: %f seconds", query.name, average(durations[query.name]))
+        finally:
+            if interval > 0:
+                # Give the service a fair break to reduce its charge
+                LOG.debug("Sleeping %d seconds...", int(QUERY_INTERVAL))
+                time.sleep(QUERY_INTERVAL)
+
+
+def read_results(filename) -> Iterator[QueryResult]:
+    if not os.path.isfile(filename):
+        return
+    with open(filename, "r", newline="") as csv_file:
+        LOG.info("Reading results from '%s'.", filename)
+        for row in csv.DictReader(csv_file):
+            yield QueryResult(
+                timestamp=row["timestamp"],
+                name=row["name"],
+                duration=float(row["duration"]),
+            )
+
+
+def write_results(filename: str, results: Iterable['QueryResult']) -> None:
+    out_dir = os.path.dirname(filename)
+    if out_dir and not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    write_header = not os.path.isfile(filename)
+    with open(filename, "a", newline="") as fd:
+        LOG.info("Writing results to '%s'.", filename)
+        writer = csv.DictWriter(fd, fieldnames=["timestamp", "name", "duration"])
+        if write_header:
+            writer.writeheader()
+        for r in results:
+            writer.writerow(dataclasses.asdict(r))
+            fd.flush()
 
 
 def average(durations: list[float]) -> float:
     if not durations:
         return 0.
     return sum(durations) / len(durations)
-
-
-def search(query: Query) -> Any:
-    url = f"{API_URL}/{query.path}".rstrip("/")
-    return client(url).search(**query.body)
 
 
 @functools.cache
